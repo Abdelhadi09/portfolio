0328e508578221dc7666e7a64439c1bb4df147cd
abdelhadi <abaditrki@gmail.com>
2024-11-02 11:33:50 -0700

    protfolio desktop version

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector(".humberger").addEventListener("click", () => {
+    document.querySelector(".taskbar").classList.toggle("active");
+    document.querySelector(".humberger").classList.toggle("gone");
+});
